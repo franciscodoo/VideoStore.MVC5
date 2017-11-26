839a10c8addeb5c609d30969ecaeb8889eafff25
franciscodoo <francisco.martim.o@gmail.com>
2017-11-26 18:05:20 +0000

    attribute-routing-with-constraint

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Les Miserables" };
+            return View(movie);
+        }
+
+        //[Route("movies/released/{year:regex(\\d{4):min(1900)}/{month:regex(\\d{2})}")]
+        [Route("movies/released/{year:min(1900)}/{month:range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content("Movies released on " + year + "/" + month + ":");
+        }
+    }
+}
