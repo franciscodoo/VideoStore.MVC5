42595998ec56f0f5741625aa5970be0ddb8c56e2
Francisco Martim <francisco.martim.o@gmail.com>
2017-11-29 00:30:46 +0000

    exercise1_draft
    exercise1_draft

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -12,13 +13,12 @@ namespace Vidly.Controllers
         // GET: Movie
         public ActionResult Index()
         {
-            return View();
-        }
-
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Les Miserables" };
-            return View(movie);
+            var movies = new List<Movie>
+            {
+                new Movie{Id = 1, Name = "Les Miserables"},
+                new Movie{Id = 2, Name = "Monty Python"}
+            };
+            return View(movies);
         }
 
         //[Route("movies/released/{year:regex(\\d{4):min(1900)}/{month:regex(\\d{2})}")]
@@ -26,6 +26,24 @@ namespace Vidly.Controllers
         public ActionResult ByReleaseDate(int year, int month)
         {
             return Content("Movies released on " + year + "/" + month + ":");
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Les Miserables" };
+            var customers = new List<Customer>
+            {
+                new Customer {Name = "Fonseca Galhão"},
+                new Customer {Name = "Sancho Paulo"},
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
         }
     }
 }
